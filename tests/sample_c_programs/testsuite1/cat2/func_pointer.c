70718451d0c7bbcc9212781a75047e0aca907382
CDACesec <102668343+CDACesec@users.noreply.github.com>
2022-06-08 17:41:25 +0530

    Update func_pointer.c

--- tests/sample_c_programs/testsuite1/cat2/func_pointer.c
+++ tests/sample_c_programs/testsuite1/cat2/func_pointer.c
@@ -1,10 +1,18 @@
-/*      NAME:  MOHIT KUMAR
-        E-CODE:343542
-        DATE:1/dec/2020
-        E-MAIL: kmohit@cdac.in
-   
-   
-*/
+/*
+ * Copyright (c) 2022 Centre for Development of Advanced Computing, Hyderabad.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 
 
