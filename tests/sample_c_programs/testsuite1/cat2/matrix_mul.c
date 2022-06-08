04759f950e0772850ec2d155ed2a309967085435
CDACesec <102668343+CDACesec@users.noreply.github.com>
2022-06-08 17:42:02 +0530

    Update matrix_mul.c

--- tests/sample_c_programs/testsuite1/cat2/matrix_mul.c
+++ tests/sample_c_programs/testsuite1/cat2/matrix_mul.c
@@ -1,10 +1,19 @@
-
 /*
-	Name     :- TIWARI ABHINAV ASHOK KUMAR
-	Emp id   :- 343813
-	Email id :-abhinavt@cdac.in
+ * Copyright (c) 2022 Centre for Development of Advanced Computing, Hyderabad.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-*/
 #include<stdio.h>
 void multiply(int mat1[12][12],int mat2[12][12],int ,int ,int );
 void main();
