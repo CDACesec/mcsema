7372164b7554dace4825b07f40228f5b132e0695
CDACesec <102668343+CDACesec@users.noreply.github.com>
2022-06-08 17:40:20 +0530

    Update file_write.c

--- tests/sample_c_programs/testsuite1/cat2/file_write.c
+++ tests/sample_c_programs/testsuite1/cat2/file_write.c
@@ -1,14 +1,20 @@
+/*
+ * Copyright (c) 2022 Centre for Development of Advanced Computing, Hyderabad.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 /* PROGRAM FOR FILE HANDLING.
-
-
-/*	NAME:  MOHIT KUMAR
-	E-CODE:343542
-	DATE:4/SEP/2019
-	E-MAIL: kmohit@cdac.in
-
-
-*/
 
 /*
      * C program to create a file called emp.rec and store information
